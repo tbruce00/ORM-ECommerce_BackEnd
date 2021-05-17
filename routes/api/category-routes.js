0d698db13784b7af5d5ef252b965a2c0640cea3f
Trevor Bruce <tbnyk03@gmail.com>
2021-05-16 19:00:22 -0600

    Fix put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,9 +45,25 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-});
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData) {
+      res.json(404).json({
+        message: 'There are no categories with that id.'
+      });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+  });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
@@ -58,7 +74,9 @@ router.delete('/:id', async (req, res) => {
       }
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'There are no categories with that id.'});
+      res.status(404).json({ 
+        message: 'There are no categories with that id.'
+      });
       return;
     }
     res.status(200).json(categoryData);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,9 +43,25 @@ router.post('/', async (req, res) => {
   }
   });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-});
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!tagData) {
+      res.json(404).json({
+        message: 'There are no tags with that id.'
+      }); 
+        return;
+      }
+      res.status(200).json(tagData);
+    } catch(err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
